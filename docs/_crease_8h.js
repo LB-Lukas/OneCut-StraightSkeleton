0e2ad41b9a10bcd33528b7eefcc59eb3c06761fb
Lukas Breuer <s98lbreu@uni-bonn.de>
2025-03-31 12:50:40 +0200

    another docs fix

---
+++ docs/_crease_8h.js
@@ -0,0 +1,13 @@
+var _crease_8h =
+[
+    [ "FoldType", "_crease_8h.html#gae0436bbd7d4cad839b4f455c478b5c84", [
+      [ "MOUNTAIN", "_crease_8h.html#gae0436bbd7d4cad839b4f455c478b5c84a9d1480267288b8146bb0756b67ddcbbe", null ],
+      [ "VALLEY", "_crease_8h.html#gae0436bbd7d4cad839b4f455c478b5c84a4e89332934c40ecd6b609da1b347da68", null ],
+      [ "UNFOLDED", "_crease_8h.html#gae0436bbd7d4cad839b4f455c478b5c84a33cc8ca0df4e2bed4d9befe7d4fcb6a7", null ]
+    ] ],
+    [ "Origin", "_crease_8h.html#ga9f8bff4c55cdeb2ab2dc130061d0036d", [
+      [ "POLYGON", "_crease_8h.html#ga9f8bff4c55cdeb2ab2dc130061d0036daecdc92bf56d960b73b02ee40125758bc", null ],
+      [ "SKELETON", "_crease_8h.html#ga9f8bff4c55cdeb2ab2dc130061d0036da057e1bb1892582c5b4b02ed27fddf4a7", null ],
+      [ "PERPENDICULAR", "_crease_8h.html#ga9f8bff4c55cdeb2ab2dc130061d0036da75e7bce029e66ec013b1ab0e84f9ad63", null ]
+    ] ]
+];
---
+++ docs/_perpendicular_finder_8h.js
@@ -0,0 +1,6 @@
+var _perpendicular_finder_8h =
+[
+    [ "PerpSegment", "struct_one_cut_1_1_perp_segment.html", "struct_one_cut_1_1_perp_segment" ],
+    [ "PerpendicularHit", "struct_one_cut_1_1_perpendicular_hit.html", "struct_one_cut_1_1_perpendicular_hit" ],
+    [ "PerpChain", "_perpendicular_finder_8h.html#a27f673b192b8b78cef26ba13c400faf1", null ]
+];
